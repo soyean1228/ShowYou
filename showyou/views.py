ac3d384829e493bfbf9515d6569bbd3090a33793
soyean1228 <soyean1228@gmail.com>
2020-05-24 19:25:32 +0900

    날짜 입력 기능

--- showyou/views.py
+++ showyou/views.py
@@ -17,22 +17,8 @@ from django.http import JsonResponse
 
 def index(request):
     # sentiment_analysis.Sentiment_Analysis()
-    keyword_wordcloud.total_wordcloud()
+    # keyword_wordcloud.total_wordcloud()
     return render(request, 'showyou/index.html') 
-
-# 
-# def data(request):     
-#     x = np.array(['2017-07-10', '2017-07-11', '2017-07-12', '2017-07-13', '2017-07-14'])
-#     # post_list = mongo_connection.post_id_find()
-#     # x = np.array(post_list)
-#     y = np.array([58.13, 53.98, 67.00, 89.70, 99.00])
-#     # myData = json.dumps([{"date": str(x[i]), "close": y[i]} for i in range(5)])
-#     myData = json.dumps([{"date": x[i] , "close": y[i]} for i in range(5)])
-#     return JsonResponse(myData, safe=False)
-
-# def sentiment(request):
-#     return render(request, 'showyou/sentiment_result.html', context=None)
-
 
 def generic(request):
     # keyword_wordcloud.total_wordcloud()
@@ -43,10 +29,13 @@ def elements(request):
 
 def twitter(request):
     search_keyword = request.GET.get('search_keyword', '')
+    date = request.GET.get('date')
+    print(date)
     if search_keyword:
         print("있는 경우")
+        print(date)
         print('search_keyword = ' + search_keyword)
-        twitter_parser_total.parsing(search_keyword,'m')
+        twitter_parser_total.parsing(search_keyword,'date')
         textmining.analysis()
         # keyword_wordcloud.show()
         return render(request, 'showyou/twitter_result.html')
@@ -56,23 +45,27 @@ def twitter(request):
 
 def twitter_user(request):
     search_keyword = request.GET.get('search_keyword', '')
+    date = request.GET.get('date')
+    print(date)
     if search_keyword:
         print("있는 경우")
         print('search_keyword = ' + search_keyword)
-        twitter_parser_total.parsing(search_keyword,'m')
+        twitter_parser_total.parsing(search_keyword,'date')
         textmining.analysis()
-        return render(request, 'showyou/twitter.html')
+        return render(request, 'showyou/twitter_user.html')
     else :
         print("없는 경우")
-        return render(request, 'showyou/twitter.html') 
+        return render(request, 'showyou/twitter_user.html') 
 
 def blog(request):
     print("blog 크롤링")
     search_keyword = request.GET.get('search_keyword', '')
+    date = request.GET.get('date')
+    print(date)
     print('search_keyword = ' + search_keyword)
     if search_keyword:
         print("있는 경우")
-        blog_parser_total.parsing(search_keyword,'m')
+        blog_parser_total.parsing(search_keyword,'date')
         # blog_parser_personal.parsing(search_keyword)
         textmining.analysis()
         return render(request, 'showyou/blog.html') 
@@ -83,32 +76,34 @@ def blog(request):
 def blog_user(request):
     print("blog 크롤링")
     search_keyword = request.GET.get('search_keyword', '')
+    date = request.GET.get('date')
+    print(date)
     print('search_keyword = ' + search_keyword)
     if search_keyword:
         print("있는 경우")
-        blog_parser_total.parsing(search_keyword,'m')
+        blog_parser_total.parsing(search_keyword,'date')
         # blog_parser_personal.parsing(search_keyword)
         textmining.analysis()
-        return render(request, 'showyou/blog.html') 
+        return render(request, 'showyou/blog_user.html') 
     else :
         print("없는 경우")
-        return render(request, 'showyou/blog.html') 
+        return render(request, 'showyou/blog_user.html') 
 
-def instagram_user(request):
-    print("instagram 크롤링")
-    search_keyword = request.GET.get('search_keyword', '')
-    print('search_keyword = ' + search_keyword)
-    if search_keyword:
-        print("있는 경우")
-        instagram_parser_personal.parsing(search_keyword)
-        textmining.analysis()
-        return render(request, 'showyou/instagram_user.html')
-    else :
-        print("없는 경우")   
-        return render(request, 'showyou/instagram_user.html')
-    
 def instagram(request):
     search_keyword = request.GET.get('search_keyword', '')
+    if search_keyword:
+        print("있는 경우")
+        print('search_keyword = ' + search_keyword)
+        twitter_parser_total.parsing(search_keyword,'m')
+        #textmining.analysis()
+        return render(request, 'showyou/instagram.html')
+    else :
+        print("없는 경우")
+        return render(request, 'showyou/instagram.html')
+    
+def instagram_user(request):
+    search_keyword = request.GET.get('search_keyword', '')
+    
     if search_keyword:
         print("있는 경우")
         print('search_keyword = ' + search_keyword)
@@ -118,3 +113,4 @@ def instagram(request):
     else :
         print("없는 경우")
         return render(request, 'showyou/instagram_user.html')
+
